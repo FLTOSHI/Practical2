96fa55cafb39d5b954343a30765ddbf2a5bed90f
FLTOSHI <fltoshimyname@gmail.com>
2023-12-27 09:52:02 +0300

    ъеъ

--- src/Num1.java
+++ src/Num1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Num1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.hasNext() ? scanner.nextInt() : 0;
+        int num = scanner.nextInt();
         System.out.println(num / 10 + " " + num % 10);
     }
 }
